60c34d73aaefab4033cbb787291da2869ea8c00c
AkeemDavidson <davidaa22@juniata.edu>
2025-12-09 11:09:29 -0500

    Add files via upload

---
+++ java/Mater.js
@@ -0,0 +1,10 @@
+function changebackground() {
+        document.body.style.background = "url('C:/Users/Keem8/OneDrive/Documents/Desktop/IT105FA25/final/media/flag.jpg')";
+        // document.body.style.backgroundSize = 'cover';
+        // document.body.style.backgroundPosition = 'center';
+}
+
+function changebackgroundimage() {
+        document.body.style.background = "none"
+        document.body.style.backgroundColor = "#321414"
+}
